a08f2e36d72c4aa9ae208c4fc1b8156264175c8f
mxgoddard <maxolivergoddard@gmail.com>
2020-09-21 12:25:22 +0100

    Initial commit

---
+++ src/Components/Placeholder.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../Styles/Placeholder.scss';
+
+class Placeholder extends Component {
+    render() {
+        return (
+            <h1>Hello, World!</h1>
+        )
+    }
+}
+
+export default Placeholder;
